7665f83b41873fbcb8526ae529a9ea6d858c6a8a
gbugaisky <gbugaisk@gmail.com>
2017-11-21 13:42:50 -0800

    More inital setup & configuration.  Move routes into a seperate directory.  Add helmet.

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const routes = require('./routes');
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("Server's working");
-});
+app.use('/', routes);
 
 app.listen(3000, () => {});
